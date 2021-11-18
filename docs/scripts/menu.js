0dac19d5e6d0fa501af2d976a37d8ec50ca87896
Doug Rosman <drosman@saic.edu>
2021-11-18 11:16:55 -0600

    add aframe demo

--- docs/scripts/menu.js
+++ docs/scripts/menu.js
@@ -1,1 +1,13 @@
-console.log("i'm menu.js")
+
+let mainMenu = `
+    <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+        <li><a href="/works">Works</a></li>
+        <li><a href="/works">Works</a></li>
+        <li><a href="/works/sample-project1">Works</a></li>
+        <li><a href="/">Home</a></li>
+    </ul>
+`
+
+$('.main-menu').append(mainMenu);
